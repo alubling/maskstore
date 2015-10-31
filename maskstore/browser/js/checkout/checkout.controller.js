66bb4e5d3398c3c1441b66503db0a181afbe51be
nyczmagix <nyczmagix@MacBook-Pro-Retina.local>
2015-10-31 15:58:30 -0400

    initial order working

--- maskstore/browser/js/checkout/checkout.controller.js
+++ maskstore/browser/js/checkout/checkout.controller.js
@@ -1,3 +1,25 @@
-app.controller('CheckoutCtrl', function($scope, ShoppingCartService) {
+app.controller('CheckoutCtrl', function($scope, ShoppingCartService, $http) {
     $scope.isCheckout = true;
+
+    $scope.submit = function() {
+        ShoppingCartService.getCart()
+            .then(function(cart) {
+                var masks = cart.getMasks();
+                var totalPrice = cart.subtotal;
+                var user = cart.userId;
+
+                var order = {
+                    masks: masks,
+                    totalPrice: totalPrice,
+                    user: user,
+                    status: "created"
+                }
+
+                return $http.post('/api/orders', order)
+                    .then(function(response) {
+                        console.log(response);
+                        return response;
+                    });
+            })
+    }
 });
